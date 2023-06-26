6ad94edac2a04dc99addff990a25ac7af7136b10
unknown <marsanic.david@gmail.com>
2023-06-26 16:37:10 +0200

    created a react frontend with nhost template, added some changes I saw online and with ChatGPT

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { NhostClient, NhostProvider } from '@nhost/react'
+import Layout from './components/Layout';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+const nhost = new NhostClient({
+  subdomain: process.env.REACT_APP_NHOST_SUBDOMAIN,
+  region: process.env.REACT_APP_NHOST_REGION
+})
+
+function App() {
+  return (
+    <NhostProvider nhost={nhost}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="sign-in" element={<SignIn />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </NhostProvider>
+  );
+}
+
+export default App;
