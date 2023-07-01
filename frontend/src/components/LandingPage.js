488e844f2887028beebfae177c48fa9a9b564656
unknown <marsanic.david@gmail.com>
2023-07-01 17:24:52 +0200

    added a logo to the landing page. also made it look better

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -2,56 +2,49 @@ import styles from '../styles/pages/LandingPage.module.css';
 
 const LandingPage = () => {
     return (
-        <div className={styles.landingPage}>
-            <header className={styles.navBar}>
-                <nav>
-                    {/* Your navigation bar contents */}
-                </nav>
-                <div className={styles.heroSection}>
-                    <h1>Unlock Your Creative Potential</h1>
-                    <p>Join our platform and transform your ideas into reality</p>
-                    <button className={styles.ctaButton}>Get Started</button>
-                </div>
-            </header>
+        <div className={styles.landing}>
+            <div className={styles.logoWrapper}>
+                <img className={styles.logo} src={process.env.PUBLIC_URL + '/logo.svg'} alt="logo" />
+            </div>
 
-            <section className={styles.aboutUs}>
-                <h2>About Us</h2>
-                <p>We are a dynamic platform committed to fostering a community where individuals can express their unique talents, learn new skills, and collaborate on innovative projects.</p>
+            <section className={styles.heroSection}>
+                <h1 className={styles.heroTitle}>Unleash Your Creative Potential</h1>
+                <p className={styles.heroText}>
+                    We believe that every individual has unique talents. Our platform is built to help you discover and express your creativity. Join us and unlock your potential.
+                </p>
+                <button className={styles.ctaButtonBig}>Get Started</button>
             </section>
 
-            <section className={styles.features}>
-                <h2>Platform Features</h2>
-                <ul>
-                    <li>Ease of use: Intuitive, user-friendly interface.</li>
-                    <li>Collaboration: Work seamlessly on any project with your team.</li>
-                    <li>Revenue Sharing: Earn money from your contributions to the platform.</li>
-                    <li>Support: Extensive resources and community support for your projects.</li>
-                </ul>
-            </section>
+            <div className={styles.sectionBox}>
+                <section className={styles.missionSection}>
+                    <h2 className={styles.sectionTitle}>Our Mission</h2>
+                    <p className={styles.sectionText}>
+                        We strive to break down barriers to creativity. Our mission is to simplify software development, foster community collaboration, provide an excellent user experience, and champion open-source as the industry standard.
+                    </p>
+                    <button className={styles.ctaButton}>Learn More</button>
+                </section>
+            </div>
 
-            <section className={styles.testimonials}>
-                <h2>Testimonials</h2>
-                <div>
-                    {/* You can add some testimonials here. It's better if you can get some real testimonials from your users */}
-                    <blockquote>
-                        <p>"This platform has transformed the way I work and collaborate on my projects"</p>
-                        <footer>- Happy User</footer>
-                    </blockquote>
-                    {/* Repeat for other testimonials */}
-                </div>
-            </section>
+            <div className={styles.sectionBox}>
+                <section className={styles.forDevelopersSection}>
+                    <h2 className={styles.sectionTitle}>For Developers</h2>
+                    <p className={styles.sectionText}>
+                        Our mission is to make it radically easier for you to become an independent software developer. Join our community of developers to learn, share, and collaborate on innovative projects.
+                    </p>
+                    <button className={styles.ctaButton}>Join as a Developer</button>
+                </section>
+            </div>
 
-            <section className={styles.joinUs}>
-                <h2>Ready to Get Started?</h2>
-                <p>Join our platform today and start creating!</p>
-                <button className={styles.ctaButton}>Join Now</button>
-            </section>
-
-            <footer className={styles.footer}>
-                {/* Your footer here */}
-            </footer>
+            <div className={styles.sectionBox}>
+                <section className={styles.forUsersSection}>
+                    <h2 className={styles.sectionTitle}>For Users</h2>
+                    <p className={styles.sectionText}>
+                        Explore a wide variety of applications built by talented developers from around the world. Contribute to projects, share your ideas, and help shape the future of the Internet.
+                    </p>
+                    <button className={styles.ctaButton}>Join as a User</button>
+                </section>
+            </div>
         </div>
-
     );
 }
 
