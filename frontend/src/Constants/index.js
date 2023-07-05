d4c24aaafb65f1c69579f166e836d4856bfad77e
unknown <marsanic.david@gmail.com>
2023-07-05 16:25:15 +0200

    imported tailwind, created constants page

---
+++ frontend/src/Constants/index.js
@@ -0,0 +1,39 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star } from "../assets";
+
+export const about = {
+    welcomeTitle: "Welcome to Our Open Source Platform",
+    welcomeSubtitle: "Redefining the Way We Interact with Technology",
+    welcomeDescription: "Welcome to Open Source Platform, a unique place that brings together developers and users in a new and exciting way. We're more than just a tech platform - we're a community that values innovation, collaboration, and shared growth.",
+
+    whatIsTitle: "What is Open Source Platform?",
+    whatIsDescription: "[Your Platform Name] is a web-based platform designed to host open-source projects. We provide a space for developers to share their work and be fairly compensated for their contributions. For users, we offer access to a wealth of innovative projects, all in one place.",
+
+    forDevelopersTitle: "For Developers",
+    forDevelopersPoints: [
+        "Becoming an Independent Developer: We simplify the process of becoming an independent developer. With our platform, you can focus on what you do best - creating innovative solutions.",
+        "Monetization Simplified: You don't have to worry about monetization schemes. We handle that for you, ensuring you're compensated for the value you bring to the community.",
+        "Ad-Free Experience: Your apps can be free of intrusive ads and restrictive free tiers. This means a better experience for your users and less hassle for you.",
+        "Real-Time User Feedback: Get immediate feedback from users. This helps you understand what users want, reducing the risk of building something that doesn't meet their needs.",
+        "Earn from Your Solutions: If others incorporate your solution into a new app, you get compensated. It's a win-win situation."
+    ],
+
+    forUsersTitle: "For Users",
+    forUsersPoints: [
+        "Ease of Use: With Open Source Platform, accessing open-source apps is as easy as clicking a button. No need to navigate through multiple platforms - it's all here.",
+        "Community-Vetted Apps: All apps on our platform are community-vetted and verified for security and privacy. You can use them with confidence.",
+        "Ad-Free and Privacy-Focused: We prioritize your privacy. Our platform is free of ads, trackers, and we don't sell your data.",
+        "All in One Place: No need to pay for dozens of apps and services. Whether you need a project management tool or a photo editing app, chances are you'll find it here.",
+        "Direct Support: Have a question or need a feature? Reach out to developers directly through our platform.",
+        "You're in Control: With our in-browser code editor, you can edit apps and customize your experience."
+    ],
+
+    missionTitle: "Our Mission",
+    missionDescription: "We're here to create a platform that benefits both users and creators. Our mission is to:",
+    missionPoints: [
+        "Protect your data and attention from being sold",
+        "Make technology accessible to everyone",
+        "Enable creators to build the apps they want to build",
+        "Ensure creators are rewarded for their contributions"
+    ],
+    joinUsStatement: "Join us today and experience the difference."
+};
