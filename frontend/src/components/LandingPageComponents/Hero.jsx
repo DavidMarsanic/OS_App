74dc1891ae71718b2a04e781351c0e2025862e7d
unknown <marsanic.david@gmail.com>
2023-07-07 17:20:01 +0200

    created hero section

--- frontend/src/components/LandingPageComponents/Hero.jsx
+++ frontend/src/components/LandingPageComponents/Hero.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
 const Hero = () => (
-    <section id='home' className=''>
-
+    <section className="bg-primary flex items-center">
+        <div className="max-w-4xl mx-auto px-4">
+            <h1 className="text-4xl font-bold text-white mt-8">Welcome to Our Website</h1>
+            <p className="text-lg text-white mt-2">
+                Discover the best products and services for your needs.
+            </p>
+            <button className="bg-white text-primary font-semibold py-2 px-4 mt-4 rounded-full">
+                Get Started
+            </button>
+        </div>
     </section>
+);
 
-)
-export default Hero
+export default Hero;
