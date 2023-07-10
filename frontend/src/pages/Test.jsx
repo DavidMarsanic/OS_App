125beacbc9b9467bfb58444135e6f31f1068fe38
unknown <marsanic.david@gmail.com>
2023-07-10 23:40:54 +0200

    added links and buttons to all the boxes on landing page

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -9,6 +9,7 @@ const boxStyle = "flex flex-col w-full col-span-1 my-2 bg-gray-800 p-6 rounded-l
 const titleStyle = "text-2xl font-semibold mb-4 w-full text-center";
 const subheadingStyle = "text-xl font-semibold mb-2";
 const textStyle = "mb-4";
+const buttonStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4";
 const imageStyle = "w-32 h-32 bg-blue-500 mx-auto";
 const missionStyle = "md:1/2";
 
@@ -32,76 +33,101 @@ export default function Test() {
             <h2 className={titleStyle}>For Developers</h2>
             <div className={sectionStyle}>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Independence</h3>
-                    <p className={textStyle}>Contribute and collaborate with developers globally.</p>
-                    <div className={imageStyle}></div>
-                </div>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Monetization</h3>
-                    <p className={textStyle}>Simplify the process of earning from your code.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-1">
+                    <button>
+                        <h3 className={subheadingStyle}>Independence</h3>
+                        <p className={textStyle}>Contribute and collaborate with developers globally.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Experience</h3>
-                    <p className={textStyle}>Enjoy an ad-free environment to work on.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-1">
+                    <button>
+                        <h3 className={subheadingStyle}>Monetization</h3>
+                        <p className={textStyle}>Simplify the process of earning from your code.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Feedback</h3>
-                    <p className={textStyle}>Get real-time feedback from users and peers.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-1">
+                    <button>
+                        <h3 className={subheadingStyle}>Experience</h3>
+                        <p className={textStyle}>Enjoy an ad-free environment to work on.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Earning</h3>
-                    <p className={textStyle}>Profit from your solutions and ideas.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-1">
+                    <button>
+                        <h3 className={subheadingStyle}>Feedback</h3>
+                        <p className={textStyle}>Get real-time feedback from users and peers.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
+
+                <Link className={boxStyle} to="/link-for-box-1">
+                    <button>
+                        <h3 className={subheadingStyle}>Earning</h3>
+                        <p className={textStyle}>Profit from your solutions and ideas.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
+
             </div>
 
             <h2 className={titleStyle}>For Users</h2>
             <div className={sectionStyle}>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Ease of Use</h3>
-                    <p className={textStyle}>Our platform is designed with simplicity in mind.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-1">
+                    <button>
+                        <h3 className={subheadingStyle}>Ease of Use</h3>
+                        <p className={textStyle}>Our platform is designed with simplicity in mind.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Community-Vetted Apps</h3>
-                    <p className={textStyle}>All apps are vetted by our community.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-2">
+                    <button>
+                        <h3 className={subheadingStyle}>Community-Vetted Apps</h3>
+                        <p className={textStyle}>All apps are vetted by our community.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Ad-Free and Privacy-Focused</h3>
-                    <p className={textStyle}>No ads, no trackers. Forever.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-3">
+                    <button>
+                        <h3 className={subheadingStyle}>Ad-Free and Privacy-Focused</h3>
+                        <p className={textStyle}>No ads, no trackers. Forever.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>All in One Place</h3>
-                    <p className={textStyle}>Find all your needs in one place.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-4">
+                    <button>
+                        <h3 className={subheadingStyle}>All in One Place</h3>
+                        <p className={textStyle}>Find all your needs in one place.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>Direct Support</h3>
-                    <p className={textStyle}>Get direct support from developers and community.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-5">
+                    <button>
+                        <h3 className={subheadingStyle}>Direct Support</h3>
+                        <p className={textStyle}>Get direct support from developers and community.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
 
-                <div className={boxStyle}>
-                    <h3 className={subheadingStyle}>You're in Control</h3>
-                    <p className={textStyle}>Have full control over your digital experience.</p>
-                    <div className={imageStyle}></div>
-                </div>
+                <Link className={boxStyle} to="/link-for-box-6">
+                    <button>
+                        <h3 className={subheadingStyle}>You're in Control</h3>
+                        <p className={textStyle}>Have full control over your digital experience.</p>
+                        <div className={imageStyle}></div>
+                    </button>
+                </Link>
             </div>
+
 
             <div className={missionStyle}>
                 <div className={boxStyle}>
@@ -113,6 +139,6 @@ export default function Test() {
             <div className="w-full flex flex-col items-center justify-center px-4 sm:px-8 py-12">
                 <p className="text-lg text-center mb-4">Join us today and experience the difference.</p>
             </div>
-        </div>
+        </div >
     );
 }
